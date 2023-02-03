e7aa5639ace5803d1e79a3e82c3f391645956f10
Tim Stack <tstack43215@yahoo.com>
2023-02-03 15:39:31 -0600

    Initial Commit

---
+++ MonteCarlo/MonteCarloApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonteCarloApp.swift
+//  MonteCarlo
+//
+//  Created by IIT PHYS 440 on 2/3/23.
+//
+
+import SwiftUI
+
+@main
+struct MonteCarloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
